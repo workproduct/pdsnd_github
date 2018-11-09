274dbcee0fef9fa31be7fbfbe462b190499e04e6
yates holley <contact@yatesholley.com>
2018-11-09 15:25:20 -0800

    style: Bikeshare.py header update
    Added Date and Author information to description header

--- bikeshare.py
+++ bikeshare.py
@@ -6,6 +6,8 @@ States—Chicago, New York City, and Washington. You will write code to import t
 data and answer interesting questions about it by computing descriptive
 statistics. You will also write a script that takes in raw input to create an
 interactive experience in the terminal to present these statistics.
+AUTHOR: Yates Holley
+DATE: 11/07/2018
 """
 
 import time
